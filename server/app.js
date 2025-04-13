b846ab8e68bdc4687aad7a17e00bc7f892c35eac
leakeyqq <leakeymugendi3@gmail.com>
2025-04-13 06:48:47 +0300

    setting up wallets

--- server/app.js
+++ server/app.js
@@ -1,41 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const authRoutes = require("./routes/auth");
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(session({
+  secret: "super-secret-key",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  },
+}));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB error:", err));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/protected", (req, res) => {
+  if (!req.session.user) return res.status(401).json({ error: "Unauthorized" });
+  res.json({ message: "You are authenticated", user: req.session.user });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
